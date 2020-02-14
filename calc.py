aedc821b8a90c9469f5c4f43c1d932bb6b0e0031
moisaniel <moisaniel@outlook.com>
2020-02-14 16:49:19 -0300

    Add files via upload
    adicionado varias opções

--- calc.py
+++ calc.py
@@ -36,41 +36,90 @@ def sub():
     
     #modifica a label mostra_resultado
     mostra_resultado.configure(text=subtrai)    
+
+#função que execulta a Multiplicação 
+def mult():
+    #lê o primeiro valor
+    valor2 = valor.get()
+    valor2 = float(valor2)
+    print("valor2", valor2) #imprime o primeiro valor
+
+    #lê o segundo valor
+    valor4 = valor3.get()
+    valor4 = float(valor4)
+    print("segundo valor", valor4) #imprime o segundo valor
     
+    #soma o segundo valor ao primeiro valor
+    vezes = valor2 * valor4
+    print("Soma =", vezes)
+    
+    #modifica a label mostra_resultado
+    mostra_resultado.configure(text=vezes)    
+
+def div():
+    #lê o primeiro valor
+    valor2 = valor.get()
+    valor2 = float(valor2)
+    print("valor2", valor2) #imprime o primeiro valor
+
+    #lê o segundo valor
+    valor4 = valor3.get()
+    valor4 = float(valor4)
+    print("segundo valor", valor4) #imprime o segundo valor
+    
+    #soma o segundo valor ao primeiro valor
+    dividir = valor2 / valor4
+    print("Soma =", dividir)
+    
+    #modifica a label mostra_resultado
+    mostra_resultado.configure(text=dividir)    
+        
+        
 win = tk.Tk()
    
    
 win.title("Calculadora Simples")
+#Cria Frame 
+frame2 = ttk.LabelFrame(win, text="botões teste2")
+frame2.grid(column=0, row=1,  padx=20, pady=20)
 #Cria a primeira label
-label = ttk.Label(win, text="digite um numero")
-label.grid(column=0, row=0)
+label = ttk.Label(frame2, text="digite um numero", width=16)
+label.grid(column=0, row=0, sticky=tk.W)
 
 #Cria a segunda label
-label2 = ttk.Label(win, text="digite um numero")
-label2.grid(column=0, row=1)
+label2 = ttk.Label(frame2, text="digite um numero", width=16)
+label2.grid(column=0, row=1, sticky=tk.W)
 
 #Cria a label resultado
-resultado_ = ttk.Label(win, text="Resultado")
-resultado_.grid(column=0, row=2)
+resultado_ = ttk.Label(frame2, text="Resultado", width=16)
+resultado_.grid(column=0, row=2,sticky=tk.W)
 
 #Cria a label mostra_resultado
-mostra_resultado = ttk.Label(win, text="=")
-mostra_resultado.grid(column=1, row=2)
+mostra_resultado = ttk.Label(frame2, text="=")
+mostra_resultado.grid(column=1, row=2, sticky=tk.W)
 
 #Cria a primeira entrada
-valor = ttk.Entry(win)
-valor.grid(column=1, row=0)
+valor = ttk.Entry(frame2, width=16)
+valor.grid(column=1, row=0, sticky=tk.W)
 
 #Cria a segunda entrada
-valor3 = ttk.Entry(win)
+valor3 = ttk.Entry(frame2)
 valor3.grid(column=1, row=1)
-
+#Frame para botões
+frame1 = ttk.LabelFrame(frame2, text="botões teste")
+frame1.grid(column=0, row=3,  padx=20, pady=40)
 #Botão de soma
-soma = ttk.Button(win, text="+", command=add)
-soma.grid(column=0, row=3)
+soma = ttk.Button(frame1, text="+", command=add)
+soma.grid(column=0, row=3, sticky=tk.W)
 #Botão de subtração
-menos = ttk.Button(win, text="-", command=sub)
-menos.grid(column=1, row=3)
+menos = ttk.Button(frame1, text="-", command=sub)
+menos.grid(column=1, row=3, sticky=tk.W)
+#Botão multiplicar
+multiplicar = ttk.Button(frame1, text="x", command=mult)
+multiplicar.grid(column=2, row=3, sticky=tk.W)
+#Botão divisão
+_dividir = ttk.Button(frame1, text="/", command=div)
+_dividir.grid(column=3, row=3, sticky=tk.W)
 #=====================
 # Start GUI
 #=====================
