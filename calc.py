cc89a788b09bb067b743f03ae2a0eafa937553b6
moisaniel <moisaniel@outlook.com>
2020-02-14 15:39:23 -0300

    add teste
    feito adição da função subtrair

--- calc.py
+++ calc.py
@@ -15,10 +15,27 @@ def add():
     #soma o segundo valor ao primeiro valor
     somatorio = valor2 + valor4
     print("Soma =", somatorio)
-    t = "a"
+    
     #modifica a label mostra_resultado
     mostra_resultado.configure(text=somatorio)
+#função que execulta a subtação 
+def sub():
+    #lê o primeiro valor
+    valor2 = valor.get()
+    valor2 = float(valor2)
+    print("valor2", valor2) #imprime o primeiro valor
+
+    #lê o segundo valor
+    valor4 = valor3.get()
+    valor4 = float(valor4)
+    print("segundo valor", valor4) #imprime o segundo valor
     
+    #soma o segundo valor ao primeiro valor
+    subtrai = valor2 - valor4
+    print("Soma =", subtrai)
+    
+    #modifica a label mostra_resultado
+    mostra_resultado.configure(text=subtrai)    
     
 win = tk.Tk()
    
@@ -48,9 +65,12 @@ valor.grid(column=1, row=0)
 valor3 = ttk.Entry(win)
 valor3.grid(column=1, row=1)
 
-
-soma = ttk.Button(win, text="soma", command=add)
-soma.grid(column=1, row=3)
+#Botão de soma
+soma = ttk.Button(win, text="+", command=add)
+soma.grid(column=0, row=3)
+#Botão de subtração
+menos = ttk.Button(win, text="-", command=sub)
+menos.grid(column=1, row=3)
 #=====================
 # Start GUI
 #=====================
